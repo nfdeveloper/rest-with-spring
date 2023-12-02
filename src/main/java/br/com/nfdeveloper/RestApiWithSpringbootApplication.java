560f875303ac07edd936471033186cc049a6bc7d
nfdeveloper <nfdeveloper04@gmail.com>
2023-12-02 14:37:50 -0300

    Criação do Projeto.

---
+++ src/main/java/br/com/nfdeveloper/RestApiWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.nfdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nfdeveloper/RestApiWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nfdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
